ce976559011bb111d22f8d9033e819beba6f3a86
sabifo4 <sandra.ac93@gmail.com>
2023-06-09 01:03:17 +0200

    Add backup main to check reproducibility

---
+++ main/pipelines_Hessian/pipeline_BASEML.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#==========================================================================================#
+# Contact Sandra Alvarez-Carretero for any doubts about this script: sandra.ac93@gmail.com #
+#==========================================================================================#
+
+# Set a `for` loop to run the tasks with as
+# many alignments as defined by the user
+
+
+
+# ------------------------------------- #
+# Creating file structure to run BASEML #
+# ------------------------------------- # 
+
+# 1. Find global dirs for paths
+pipeline_dir=$( pwd )
+main_dir=$( echo $pipeline_dir | sed 's/\/main\/..*/\/main\//' )
+cd $main_dir/Hessian/1
+home_dir=$( pwd ) 
+
+# 2. Create specific log file
+exec 3>&1> >(while read line; do echo "$line" >> $pipeline_dir/log.hessian.dir1.txt; done;) 2>&1
+start=`date`
+echo Job starts":" $start
+
+# 3. Start analysis
+echo The analyses will take place in directory $home_dir
+printf "\n"
+# Move to analysis dir
+cd $home_dir
+# Soft link the tmp* files here 
+ln -s $home_dir/prepare_baseml/tmp0001.ctl .
+ln -s $home_dir/prepare_baseml/tmp0001.trees .
+ln -s $home_dir/prepare_baseml/tmp0001.txt .
+
+# 4. Run BASEML
+printf "\nRunning BASEML to calculate the Hessian and the gradient...\n"
+baseml tmp0001.ctl
+
+# 5. Close
+printf "\n"
+echo BASEML FINISHED"!"
+printf "\n"
+end=`date`
+echo Job ends":" $end
+
